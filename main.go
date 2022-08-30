5ba71dbe6202575de8152e6885a2ff2324f0f811
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-08-30 10:48:34 +0200

    Implement multipart/form uploads Fixes zikani03/calendago#2

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"text/template"
 )
 
+var (
+	templateFile = template.Must(template.ParseFiles("static/index.html"))
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	templateFile.ExecuteTemplate(w, "index.html", nil)
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, fileHeader, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Bad Requeat", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	filename := path.Base(fileHeader.Filename)
+	dest, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer dest.Close()
+
+	if _, err = io.Copy(dest, file); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/?Succes=true", http.StatusSeeOther)
+}
 func main() {
+	http.HandleFunc("/", uploadFile)
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: implement me!
-		http.Error(w, fmt.Errorf("not implemented").Error(), http.StatusInternalServerError)
-	})
-
+	log.Println("Server started")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
